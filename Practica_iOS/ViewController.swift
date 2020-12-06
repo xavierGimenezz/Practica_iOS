3750dbdc85067c06c7e1769ebbae8e80a08b60c2
Xavier <Xavier.a.gimenez@gmail.com>
2020-12-06 11:51:31 +0100

    Initial Commit

---
+++ Practica_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica_iOS
+//
+//  Created by Adelais Caulfield on 6/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
